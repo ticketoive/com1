edc219ab7a09f5569e24c9798bfefbab7d36672d
ticketoive <helohelo1226@gmail.com>
2026-08-14 14:26:00 +0900

    Add files via upload

---
+++ 1.js
@@ -0,0 +1,19 @@
+// ==UserScript==
+// @name Test
+// @match https://ticket.tv-asahi.co.jp/tp/ticket/access/QbP9lLvsShdV5GvwaN3hmFugbcQ9F1HbdVEIXDtbsXb7bjQFXe3f8sbXeETYSbnS/*
+// ==/UserScript==
+
+(function () {
+
+'use strict';
+
+document.querySelectorAll('.ticket-text-area span').forEach(el => {
+  if (el.textContent.includes('■氏名：')) {
+    el.innerHTML = el.innerHTML.replace(
+      /■氏名：[^<]+/,
+      '■氏名：山田 太郎'
+    );
+  }
+});
+
+})();
